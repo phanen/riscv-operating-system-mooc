e3b9e55dfe939254b29524735ca40a0dec54c986
phancb <nxhsdu@qq.com>
2023-10-16 12:58:11 +0800

    workaround for compile

--- code/common.mk
+++ code/common.mk
@@ -1,5 +1,5 @@
-CROSS_COMPILE = riscv64-unknown-elf-
-CFLAGS = -nostdlib -fno-builtin -march=rv32ima -mabi=ilp32 -g -Wall
+CROSS_COMPILE = riscv64-elf-
+CFLAGS = -nostdlib -fno-builtin -march=rv32gc -mabi=ilp32 -g -Wall
 
 QEMU = qemu-system-riscv32
 QFLAGS = -nographic -smp 1 -machine virt -bios none
